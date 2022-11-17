d2e196d66f565dedb44f738aa5a67bd304f14e88
shuend <nanawoakira@gmail.com>
2022-11-17 10:22:17 +0700

    fix header

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from '@/App.vue'
+import './script.js'
 import router from '@/router'
 import '@/styles/index.css'
 import '@/styles/app.css'
